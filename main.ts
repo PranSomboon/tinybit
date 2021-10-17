8f642f3a1b7dc5c6f5cc34ebd5fad2272c325e6b
Pran <83806044+PranSomboon@users.noreply.github.com>
2021-10-17 16:04:31 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 basic.forever(function () {
     if (Tinybit.Ultrasonic_Car() < 20) {
         Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
+    } else if (Tinybit.Ultrasonic_Car() < 10) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Back)
     } else {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
     }
