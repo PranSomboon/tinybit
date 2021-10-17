76902e5ad0d99e35815d7ec69e68cd90af781509
Pran <83806044+PranSomboon@users.noreply.github.com>
2021-10-17 17:43:30 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
+loops.everyInterval(15000, function () {
+    Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
+})
 basic.forever(function () {
-    if (Tinybit.Ultrasonic_Car() < 20) {
-        Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
-    } else if (Tinybit.Ultrasonic_Car() < 10) {
+    if (Tinybit.Ultrasonic_Car() < 15) {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Back)
+    } else if (Tinybit.Ultrasonic_Car() < 25) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
+    } else if (Tinybit.Ultrasonic_Car() < 30) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 103)
+    } else if (Tinybit.Ultrasonic_Car() < 50) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 158)
     } else {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
     }
 })
