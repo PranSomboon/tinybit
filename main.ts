b702e90918a2b58b749d37fc6c2a7732012c3d95
Pran <83806044+PranSomboon@users.noreply.github.com>
2021-10-17 15:55:15 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,7 @@ let disttant = 0
 basic.forever(function () {
     disttant = Tinybit.Ultrasonic_Car()
     if (disttant < 10) {
-        while (true) {
-            Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
-            if (!(disttant < 10)) {
-                break;
-            }
-        }
+        Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
     } else {
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
     }
