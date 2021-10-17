b3c4b34e6a5ec74d55e6d9f32406d71d87b4f55e
Pran <83806044+PranSomboon@users.noreply.github.com>
2021-10-17 13:13:47 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 basic.forever(function () {
-	
+    if (Tinybit.Ultrasonic_Car() <= 3) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+    } else {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 0)
+    }
 })
