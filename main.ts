8644c9131e4834bd13ca3a744990163829f20790
Pran <83806044+PranSomboon@users.noreply.github.com>
2021-10-19 11:18:26 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,5 @@
 let can_dace = 0
 basic.forever(function () {
-    music.playMelody("C5 B A G F E D C ", 120)
-    music.playMelody("C D E F G A B C5 ", 120)
-    music.playMelody("C5 B A G G A D C ", 120)
-    music.playMelody("C D E F G F B C5 ", 120)
-    music.playMelody("C D C D C D C D ", 120)
-    music.playMelody("D E D E D E D E ", 120)
-    music.playMelody("E F E F E F E F ", 120)
-    music.playMelody("G F G F G F G F ", 120)
-    music.playMelody("G A G A G A G A ", 120)
-    music.playMelody("B A B A B A B A ", 120)
-    music.playMelody("B C5 B C5 B C5 B C5 ", 120)
-    music.playMelody("C5 B A G F E D C ", 120)
-})
-basic.forever(function () {
     music.playMelody("C D E F G A B C5 ", 120)
     music.playMelody("C5 B A G F E D C ", 120)
     music.playMelody("C D E F F E B C5 ", 120)
