ade23b7617cdcded027e3cf1d1a392fd77990a85
Pran <83806044+PranSomboon@users.noreply.github.com>
2021-12-28 15:41:14 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,50 +1,35 @@
-let can_dace = 0
-basic.forever(function () {
-    music.playMelody("C D E F G A B C5 ", 120)
-    music.playMelody("C5 B A G F E D C ", 120)
-    music.playMelody("C D E F F E B C5 ", 120)
-    music.playMelody("C5 B A G F G D C ", 120)
-    music.playMelody("C D C D C D C D ", 120)
-    music.playMelody("D E D E D E D E ", 120)
-    music.playMelody("E F E F E F E F ", 120)
-    music.playMelody("G F G F G F G F ", 120)
-    music.playMelody("G A G A G A G A ", 120)
-    music.playMelody("B A B A B A B A ", 120)
-    music.playMelody("B C5 B C5 B C5 B C5 ", 120)
-    music.playMelody("C5 B A G F E D C ", 120)
-})
 basic.forever(function () {
     if (Tinybit.Ultrasonic_Car() < 20) {
-        can_dace = 0
         Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
         Tinybit.CarCtrl(Tinybit.CarState.Car_Back)
     } else if (Tinybit.Ultrasonic_Car() < 25) {
-        can_dace = 0
-        Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
+        if (randint(0, 1) == 0) {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
+        } else {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_SpinLeft)
+        }
     } else if (Tinybit.Ultrasonic_Car() < 30) {
-        can_dace = 0
-        Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 103)
-    } else if (Tinybit.Ultrasonic_Car() < 50) {
-        can_dace = 0
+        if (randint(0, 1) == 0) {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_Right)
+        } else {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_Left)
+        }
+    } else if (Tinybit.Ultrasonic_Car() < 35) {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 86)
+    } else if (Tinybit.Ultrasonic_Car() < 50) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 103)
     } else if (Tinybit.Ultrasonic_Car() < 60) {
-        can_dace = 0
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 125)
     } else {
-        can_dace = 1
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
     }
 })
-loops.everyInterval(5000, function () {
-    if (can_dace == 1) {
-        if (randint(1, 4) == 1) {
-            Tinybit.CarCtrl(Tinybit.CarState.Car_SpinLeft)
-        } else if (randint(1, 4) == 1) {
-            Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
-        }
+basic.forever(function () {
+    if (input.lightLevel() < 50) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
     }
 })
