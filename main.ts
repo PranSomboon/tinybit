d88a7f30572fbf0ec8f9fbdc424cab8eb131ee23
Pran <83806044+PranSomboon@users.noreply.github.com>
2021-12-28 19:45:26 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,21 +16,28 @@ basic.forever(function () {
         }
     } else if (Tinybit.Ultrasonic_Car() < 35) {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 56)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 86)
     } else if (Tinybit.Ultrasonic_Car() < 50) {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 76)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 103)
     } else if (Tinybit.Ultrasonic_Car() < 60) {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 145)
-    } else if (Tinybit.Ultrasonic_Car() < 75) {
-        Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 125)
     } else {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
-        if (!(input.compassHeading() == 0)) {
+        if (randint(1, 6) == 1) {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_SpinLeft)
+        } else if (randint(1, 6) == 2) {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
+        } else if (randint(1, 6) == 3) {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_Left)
+        } else if (randint(1, 6) == 4) {
             Tinybit.CarCtrl(Tinybit.CarState.Car_Right)
+        } else if (randint(1, 6) == 5) {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+        } else if (randint(1, 6) == 6) {
+            Tinybit.CarCtrl(Tinybit.CarState.Car_Back)
         }
     }
 })
